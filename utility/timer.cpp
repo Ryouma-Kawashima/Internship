0fced20f00d63797d7cc5b00d637a9b427841042
Ryouma-Kawashima <91711951+Ryouma-Kawashima@users.noreply.github.com>
2021-10-05 16:55:02 +0900

    初期化

--- utility/timer.cpp
+++
@@ -1,9 +0,0 @@
-// -----------------------------------
-// Author: Ryouma Kawashima
-// Create: 2021/10/04
-// Update: 2021/10/04
-// -----------------------------------
-#include "../utility/params.h"
-#include "timer.h"
-
-
--- window/auto_switch.cpp
+++
@@ -1,8 +0,0 @@
-// -----------------------------------
-// Author: Ryouma Kawashima
-// Create: 2021/10/04
-// Update: 2021/10/04
-// -----------------------------------
-#include <string>
-
-#include "./auto_switch.h"
